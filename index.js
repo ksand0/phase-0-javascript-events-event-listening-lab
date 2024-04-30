ca85d9f98e3759faa1ece3ca610a01afc9d86abf
Avleen Sandhu <avleens28@gmail.com>
2024-04-30 00:05:53 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const input = document.getElementById('button');
-
 function clickAlert() {
   alert('I was clicked!');
 }
 
-input.addEventListener('click', clickAlert);
+function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', clickAlert);
+}
